2903b1153c65d2c8296535249e9f4939b26c94b4
Oleg'Ease'Kharchuk <olegease@gmail.com>
2025-12-31 23:52:16 +0200

    add integer aliases

--- include/son8/cyrillic/alias.hxx
+++ include/son8/cyrillic/alias.hxx
@@ -1,11 +1,23 @@
 #ifndef SON8_CYRILLIC_ALIAS_HXX
 #define SON8_CYRILLIC_ALIAS_HXX
 
+#include <cstddef> // size_t
+#include <cstdint> // u?int*
 #include <string>
 #include <string_view>
 
 namespace son8::cyrillic {
-
+    // integer aliases
+    using Size = std::size_t;
+    using Int0 = std::int8_t;
+    using Int1 = std::int16_t;
+    using Int2 = std::int32_t;
+    using Int3 = std::int64_t;
+    using Unt0 = std::uint8_t;
+    using Unt1 = std::uint16_t;
+    using Unt2 = std::uint32_t;
+    using Unt3 = std::uint64_t;
+    // string aliases
     using StringByte = std::string;
     using StringWord = std::u16string;
     using StringByteView = std::string_view;
